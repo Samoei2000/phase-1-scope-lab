4008b3629c3e43cedcafcfb0de9466ad56d7b9b1
Your Name <princesslorna2000@gmail.com>
2024-07-04 18:39:38 +0300

    the scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+
+var customerName = 'bob';
+
+//change to uppercase
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    bestCustomer= 'maybe bob'
+}
+
+
+//change least favorite customer
+const leastFavoriteCustomer = 'Lorna';
+
+function changeLeastFavoriteCustomer() {
+    
+    leastFavoriteCustomer = 'Cheptoo';  
+}
+changeLeastFavoriteCustomer();
